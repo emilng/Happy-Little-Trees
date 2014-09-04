399b86fcb26b2863cbd6f6ad6ce573bf97065533
Emil Ng <github@alphabeticaldisorder.com>
2014-09-04 14:50:48 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,110 @@
+module.exports = function (grunt) {
+  'use strict';
+  // Project configuration
+  grunt.initConfig({
+    // Metadata
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+    // Task configuration
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/gat.js'],
+        dest: 'dist/gat.js'
+      }
+    },
+    browserify: {
+      client: {
+        src: ['lib/**/*.js','src/**/*.js'],
+        dest: 'dist/gat.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/gat.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        node: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        eqnull: true,
+        browser: true,
+        globals: { jQuery: true },
+        boss: true
+      },
+      gruntfile: {
+        src: 'gruntfile.js'
+      },
+      lib_test: {
+        src: ['test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+    cssUrlEmbed: {
+      encodeDirectly: {
+        options: {
+          baseDir: '.'
+        },
+        files: {
+          'dist/main.css': ['styles/main.css']
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-css-url-embed');
+
+  // Default task
+  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('production', ['jshint', 'browserify', 'uglify']);
+  grunt.registerTask('css', ['cssUrlEmbed']);
+};
+
---
+++ src/gat.js
@@ -0,0 +1,8 @@
+// Genarative Art Tool
+
+var Stats = require('../lib/stats.js');
+var pixi = require('../lib/pixi.dev.js');
+
+window.onload = function(event){
+
+}
